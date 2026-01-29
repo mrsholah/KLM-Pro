eaaf59d3e81e5415f394faa977c0d2a16ec5ba0a
mrsholah <kecantikanwajah25@gmail.com>
2026-01-29 22:45:44 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
+
+self.addEventListener("notificationclick", e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow("/"));
+});
